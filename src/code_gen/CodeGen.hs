5063508375a3d02da28423d3e3f2b79b2c7ba4c8
dan soucy <dev@danso.ca>
2019-12-08 21:43:23 -0500

    rename valueable to generatable

--- src/code_gen/CodeGen.hs
+++ src/code_gen/CodeGen.hs
@@ -2,14 +2,14 @@ module CodeGen (generate) where
 
 import SouC_Types
 
-class Valueable a where
-    value :: a -> String
+class Generatable a where
+    gen :: a -> String
 
-instance Valueable Identifier where
-    value (Identifier v) = v
+instance Generatable Identifier where
+    gen (Identifier v) = v
 
-instance Valueable Raw_Expr where
-    value (Raw_Expr v) = v
+instance Generatable Raw_Expr where
+    gen (Raw_Expr v) = v
 
 
 generate :: Program -> String
@@ -20,20 +20,20 @@ generate (Program _ _ body) = includes ++ concat (map generate_top_level body)
 
 generate_param :: Param -> String
 generate_param [] = ""
-generate_param [param] = "int " ++ value param -- FIXME
+generate_param [param] = "int " ++ gen param -- FIXME
 generate_param _ = "FIXME LOL"
 
 generate_top_level :: Top_Level_Defn -> String
 generate_top_level (Top_Level_Const_Defn name raw_expr) =
-    "const int " ++ value name ++ " = " ++ value raw_expr ++ ";\n"
+    "const int " ++ gen name ++ " = " ++ gen raw_expr ++ ";\n"
 generate_top_level (FuncDefn name param stmts) =
-    "int " ++ value name ++ "(" ++ generate_param param ++ ") {" ++ body ++ "}\n"
+    "int " ++ gen name ++ "(" ++ generate_param param ++ ") {" ++ body ++ "}\n"
         where body = generate_stmts stmts
 generate_top_level (ShortFuncDefn name param raw_expr) =
-    "int " ++ value name ++ "(" ++ generate_param param  ++ ") { return " ++
-    value raw_expr ++ "; }\n"
+    "int " ++ gen name ++ "(" ++ generate_param param  ++ ") { return " ++
+    gen raw_expr ++ "; }\n"
 generate_top_level (SubDefn name m_param stmts) =
-    "void " ++ value name ++ "(" ++ param ++ ") { " ++ body ++ "}\n" where
+    "void " ++ gen name ++ "(" ++ param ++ ") { " ++ body ++ "}\n" where
         body = generate_stmts stmts
         param = case m_param of
             Nothing -> "void"
@@ -52,32 +52,32 @@ generate_stmt :: Stmt -> String
 generate_stmt (Stmt_Return m_raw_expr) = "return " ++ raw_expr ++ "; " where
     raw_expr = case m_raw_expr of
         Nothing -> ""
-        Just e -> value e
+        Just e -> gen e
 generate_stmt (Stmt_Sub_Call name m_raw_expr) =
-    value name ++ "(" ++ raw_expr ++ "); " where
+    gen name ++ "(" ++ raw_expr ++ "); " where
         raw_expr = case m_raw_expr of
             Nothing -> ""
-            Just e -> value e
+            Just e -> gen e
 generate_stmt (Stmt_Var_Assign name raw_expr) =
-    "int " ++ value name ++ " = " ++ value raw_expr ++ "; "
+    "int " ++ gen name ++ " = " ++ gen raw_expr ++ "; "
 generate_stmt (Stmt_Const_Assign name raw_expr) =
-    "const int " ++ value name ++ " = " ++ value raw_expr ++ "; "
-generate_stmt (Stmt_Postfix_Oper name oper) = value name ++ oper ++ "; "
+    "const int " ++ gen name ++ " = " ++ gen raw_expr ++ "; "
+generate_stmt (Stmt_Postfix_Oper name oper) = gen name ++ oper ++ "; "
 generate_stmt (Stmt_While raw_expr stmts) =
-    "while ( " ++ value raw_expr ++ " ) { " ++ generate_stmts stmts ++ "} "
+    "while ( " ++ gen raw_expr ++ " ) { " ++ generate_stmts stmts ++ "} "
 generate_stmt (Stmt_Until raw_expr stmts) =
-    "while (!( " ++ value raw_expr ++ " )) { " ++ generate_stmts stmts ++ "} "
+    "while (!( " ++ gen raw_expr ++ " )) { " ++ generate_stmts stmts ++ "} "
 generate_stmt (Stmt_If raw_expr stmts m_else_stmts) =
     if_branch ++ else_branch m_else_stmts where
         if_branch =
-            "if ( " ++ value raw_expr ++ " ) { " ++ generate_stmts stmts ++ "} "
+            "if ( " ++ gen raw_expr ++ " ) { " ++ generate_stmts stmts ++ "} "
         else_branch Nothing = ""
         else_branch (Just else_stmts) =
             "else {" ++ generate_stmts else_stmts ++ "} "
 generate_stmt (Stmt_Unless raw_expr stmts m_else_stmts) =
     if_branch ++ else_branch m_else_stmts where
         if_branch =
-            "if (!( " ++ value raw_expr ++ " )) { " ++ generate_stmts stmts ++ "} "
+            "if (!( " ++ gen raw_expr ++ " )) { " ++ generate_stmts stmts ++ "} "
         else_branch Nothing = ""
         else_branch (Just else_stmts) =
             "else {" ++ generate_stmts else_stmts ++ "} "
