4abec86cd9a20469ab2b869aa505ca310aeeb50c
dan soucy <dev@danso.ca>
2020-01-12 23:01:06 -0500

    remove code for generating raw_exprs

--- src/CodeGen/CodeGen.hs
+++ src/CodeGen/CodeGen.hs
@@ -7,7 +7,6 @@ import Common (
     Identifier(..),
     Stmts(..),
     Program(..),
-    Raw_Expr(..),
     Top_Level_Defn(..)
     )
 import Parser.ExprParser
@@ -27,23 +26,20 @@ instance Generatable ASTree where
 instance Generatable Identifier where
     gen (Identifier v) = v
 
-instance Generatable Raw_Expr where
-    gen (Raw_Expr v) = v
-
 instance Generatable Param where
     gen (Param []) = ""
     gen (Param [param]) = "int " ++ gen param -- FIXME
     gen _ = "FIXME LOL"
 
 instance Generatable Top_Level_Defn where
-    gen (Top_Level_Const_Defn name raw_expr) =
-        "const int " ++ gen name ++ " = " ++ gen raw_expr ++ ";\n"
+    gen (Top_Level_Const_Defn name expr) =
+        "const int " ++ gen name ++ " = " ++ gen expr ++ ";\n"
     gen (FuncDefn name param stmts) =
         "int " ++ gen name ++ "(" ++ gen param ++ ") {" ++ body ++ "}\n"
             where body = gen stmts
-    gen (ShortFuncDefn name param raw_expr) =
+    gen (ShortFuncDefn name param expr) =
         "int " ++ gen name ++ "(" ++ gen param  ++ ") { return " ++
-        gen raw_expr ++ "; }\n"
+        gen expr ++ "; }\n"
     gen (SubDefn name m_param stmts) =
         "void " ++ gen name ++ "(" ++ param ++ ") { " ++ body ++ "}\n" where
             body = gen stmts
@@ -61,35 +57,35 @@ instance Generatable Stmts where
     gen (Stmts stmts) = concat $ map gen stmts
 
 instance Generatable Stmt where
-    gen (Stmt_Return m_raw_expr) = "return " ++ raw_expr ++ "; " where
-        raw_expr = case m_raw_expr of
+    gen (Stmt_Return m_expr) = "return " ++ expr ++ "; " where
+        expr = case m_expr of
             Nothing -> ""
             Just e -> gen e
-    gen (Stmt_Sub_Call name m_raw_expr) =
-        gen name ++ "(" ++ raw_expr ++ "); " where
-            raw_expr = case m_raw_expr of
+    gen (Stmt_Sub_Call name m_expr) =
+        gen name ++ "(" ++ expr ++ "); " where
+            expr = case m_expr of
                 Nothing -> ""
                 Just e -> gen e
-    gen (Stmt_Var_Assign name raw_expr) =
-        "int " ++ gen name ++ " = " ++ gen raw_expr ++ "; "
-    gen (Stmt_Const_Assign name raw_expr) =
-        "const int " ++ gen name ++ " = " ++ gen raw_expr ++ "; "
+    gen (Stmt_Var_Assign name expr) =
+        "int " ++ gen name ++ " = " ++ gen expr ++ "; "
+    gen (Stmt_Const_Assign name expr) =
+        "const int " ++ gen name ++ " = " ++ gen expr ++ "; "
     gen (Stmt_Postfix_Oper name oper) = gen name ++ oper ++ "; "
-    gen (Stmt_While raw_expr stmts) =
-        "while ( " ++ gen raw_expr ++ " ) { " ++ gen stmts ++ "} "
-    gen (Stmt_Until raw_expr stmts) =
-        "while (!( " ++ gen raw_expr ++ " )) { " ++ gen stmts ++ "} "
-    gen (Stmt_If raw_expr stmts m_else_stmts) =
+    gen (Stmt_While expr stmts) =
+        "while ( " ++ gen expr ++ " ) { " ++ gen stmts ++ "} "
+    gen (Stmt_Until expr stmts) =
+        "while (!( " ++ gen expr ++ " )) { " ++ gen stmts ++ "} "
+    gen (Stmt_If expr stmts m_else_stmts) =
         if_branch ++ else_branch m_else_stmts where
             if_branch =
-                "if ( " ++ gen raw_expr ++ " ) { " ++ gen stmts ++ "} "
+                "if ( " ++ gen expr ++ " ) { " ++ gen stmts ++ "} "
             else_branch Nothing = ""
             else_branch (Just else_stmts) =
                 "else {" ++ gen else_stmts ++ "} "
-    gen (Stmt_Unless raw_expr stmts m_else_stmts) =
+    gen (Stmt_Unless expr stmts m_else_stmts) =
         if_branch ++ else_branch m_else_stmts where
             if_branch =
-                "if (!( " ++ gen raw_expr ++ " )) { " ++ gen stmts ++ "} "
+                "if (!( " ++ gen expr ++ " )) { " ++ gen stmts ++ "} "
             else_branch Nothing = ""
             else_branch (Just else_stmts) =
                 "else {" ++ gen else_stmts ++ "} "
