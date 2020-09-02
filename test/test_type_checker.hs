6e71fd6e639d9479b0391029ad8c3243960d7072
dan soucy <dev@danso.ca>
2020-09-01 20:36:00 -0400

    add more typechecker tests

--- test/test_type_checker.hs
+++ test/test_type_checker.hs
@@ -27,12 +27,24 @@ mismatch x y = Result $ Just $ TypeError x y
 empty_ctx :: Context
 empty_ctx = Global []
 
+globals :: Context
+globals = Global [
+    Bound (Identifier "x") (TypeName "Int"),
+    Bound (Identifier "s") (TypeName "String"),
+    Bound (Identifier "c") (TypeName "Char"),
+    Bound (Identifier "b") (TypeName "Bool")
+    ]
+
 tests :: [Test]
 tests = [
     (empty_ctx, Leaf (LitInt 3), "Integer", match, "int"),
     (empty_ctx, Leaf (LitChar 'a'), "Char", match, "char"),
     (empty_ctx, Leaf (LitString "what"), "String", match, "string"),
-    (empty_ctx, Leaf (LitBool True), "Bool", match, "bool")
+    (empty_ctx, Leaf (LitBool True), "Bool", match, "bool"),
+    (globals, Leaf (Var "x" Nothing), "Int", match, "intvar"),
+    (globals, Leaf (Var "s" Nothing), "String", match, "stringvar"),
+    (globals, Leaf (Var "c" Nothing), "Char", match, "charvar"),
+    (globals, Leaf (Var "b" Nothing), "Bool", match, "boolvar")
     ]
 
 borked_tests :: [Test]
@@ -40,7 +52,11 @@ borked_tests = [
     (empty_ctx, Leaf (LitInt 3), "Char", mismatch "Char" "Integer", "int"),
     (empty_ctx, Leaf (LitChar 'a'), "Int", mismatch "Int" "Char", "char"),
     (empty_ctx, Leaf (LitString "what"), "Char", mismatch "Char" "String", "string"),
-    (empty_ctx, Leaf (LitBool True), "Char", mismatch "Char" "Bool", "bool")
+    (empty_ctx, Leaf (LitBool True), "Char", mismatch "Char" "Bool", "bool"),
+    (globals, Leaf (Var "x" Nothing), "Char", mismatch "Char" "Int", "intvar"),
+    (globals, Leaf (Var "s" Nothing), "Bool", mismatch "Bool" "String", "stringvar"),
+    (globals, Leaf (Var "c" Nothing), "String", mismatch "String" "Char", "charvar"),
+    (globals, Leaf (Var "b" Nothing), "Char", mismatch "Char" "Bool", "charvar")
     ]
 
 
