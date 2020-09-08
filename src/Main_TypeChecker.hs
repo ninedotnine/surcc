15c9eaed5289a1ef6863d137cb8193fb825831ce
dan soucy <dev@danso.ca>
2020-09-07 20:27:41 -0400

    improve type-checker binary

--- src/Main_TypeChecker.hs
+++ src/Main_TypeChecker.hs
@@ -1,29 +1,57 @@
 module Main_TypeChecker where
 
+import Data.Foldable (traverse_)
+import System.Directory
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 
 import Parser.SouCParser
 import TypeChecker.TypeChecker
 
-getFileData :: IO (FilePath, String)
-getFileData = getArgs >>= \args -> if length args < 1
-    then do
-        contents <- getContents
-        putStrLn "no file name provided, reading from stdin."
-        return ("stdin", contents)
-    else let name = head args in do
-        contents <- readFile name
-        return (name, contents)
-
-
 main :: IO ()
 main = do
-    (filename, input) <- getFileData
+    args <- getArgs
+    if length args < 1
+        then do
+            contents <- getContents
+            putStrLn "no file name provided, reading from stdin."
+            test "stdin" contents
+        else process_args args
+
+
+process_args :: [String] -> IO ()
+process_args = traverse_ process_arg
+
+process_arg :: String -> IO ()
+process_arg arg = do
+    is_dir <- doesDirectoryExist arg
+    if is_dir
+        then process_dir arg
+        else process_file arg
+
+process_dir :: FilePath -> IO ()
+process_dir dir = do
+    cwd <- getCurrentDirectory
+    ls <- listDirectory dir
+    setCurrentDirectory dir
+    traverse_ process_file ls
+    setCurrentDirectory cwd
+
+
+process_file :: FilePath -> IO ()
+process_file name = do
+    contents <- readFile name
+    test name contents
+
+
+test :: FilePath -> String -> IO ()
+test filename input = do
+    putStr filename
+    putStr "... "
     case runSouCParser filename input of
         Left err -> do
             putStrLn (show err)
             exitFailure
         Right prog_tree -> case type_check prog_tree of
             Left typecheck_error -> print typecheck_error >> exitFailure
-            Right _ -> putStrLn ":^)"
+            Right _ -> putStrLn "OK."
