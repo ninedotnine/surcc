38868af88a05d70368ffd9d2bd3e5f4721b9317c
dan soucy <dev@danso.ca>
2020-11-01 23:05:02 -0500

    remove commented code

--- src/TypeChecker/Context.hs
+++ src/TypeChecker/Context.hs
@@ -27,7 +27,6 @@ class ContextClass c where
 
 data Builtins = Builtins [Bound] deriving Show
 data ExportList = ExportList [Bound] Builtins deriving Show
--- data GlobalScope = GlobalScope [Bound] ExportList deriving Show
 data LocalScope = GlobalScope [Bound] ExportList
                 | OuterScope [Bound] LocalScope
                 | InnerScope [Bound] LocalScope
@@ -41,11 +40,6 @@ instance ContextClass ExportList where
         Nothing -> lookup ctx ident
         just_type -> just_type
 
--- instance ContextClass GlobalScope where
---     lookup (GlobalScope bounds ctx) ident = case lookup_b bounds ident of
---         Nothing -> lookup ctx ident
---         just_type -> just_type
---
 instance ContextClass LocalScope where
     lookup (GlobalScope bounds ctx) ident = case lookup_b bounds ident of
         Nothing -> lookup ctx ident
@@ -82,8 +76,6 @@ add_bind ctx (BindMayExist modifiable) (Bound i t) = case lookup ctx i of
 
 define_export :: LocalScope -> Bound -> Either TypeError LocalScope
 define_export scope b = case scope of
---     InnerScope binds ctx -> define_export ctx b
---     OuterScope binds ctx -> define_export ctx b
     InnerScope _ _ -> error "should not define exports from inner scope"
     OuterScope _ _ -> error "should not define exports from within scope"
     GlobalScope binds ctx -> case b of
@@ -100,16 +92,12 @@ remove_export_wrapper scope  b = case scope of
         Right ok_ctx -> Right (GlobalScope bounds ok_ctx)
 
 remove_export :: ExportList -> Bound -> Either TypeError ExportList
--- remove_export (GlobalScope bounds ctx) b = case remove_export ctx b of
---     Left err -> Left err
---     Right ok_ctx -> Right (GlobalScope bounds ok_ctx)
 remove_export (ExportList bounds ctx) (Bound i t) = case lookup_b bounds i of
     Just b_t | t == b_t -> Right (ExportList (filter (\(Bound b_i _) -> b_i /= i) bounds) ctx)
     Just b_t -> Left (TypeMismatch b_t t)
     Nothing -> Left (Undeclared i)
 
 add_potential_export :: Bound -> Checker ()
--- add_potential_export :: ContextClass c => Bound -> State c (Either TypeError ())
 add_potential_export bound = do
     ctx <- get
     case remove_export_wrapper ctx bound of
@@ -120,17 +108,11 @@ add_potential_export bound = do
             Right new_ctx -> put new_ctx >> pure (Right ())
 
 
--- exports_remaining :: ContextClass c => c -> [Bound]
 exports_remaining :: LocalScope -> [Bound]
--- exports_remaining (Scoped _ ctx) = exports_remaining ctx
 exports_remaining (InnerScope _ ctx) = exports_remaining ctx
 exports_remaining (OuterScope _ ctx) = exports_remaining ctx
 exports_remaining (GlobalScope _ (ExportList [] _)) = []
 exports_remaining (GlobalScope _ (ExportList bs _)) = bs
--- exports_remaining (Builtins _) = error "should be unreachable"
-
--- type Checker a = State Context (Either TypeError a)
--- type Checker a = ContextClass c => State c (Either TypeError a)
 
 type Checker a = State LocalScope (Either TypeError a)
 
@@ -138,14 +120,12 @@ insert :: BindMayExist -> Bound -> Checker ()
 insert = insert_local
 
 insert_global :: Bound -> State LocalScope (Either TypeError ())
--- insert_global :: Bound -> State GlobalScope (Either TypeError ())
 insert_global bound = do
     ctx <- get
     case (add_bind ctx (BindMayExist False) bound) of
         Left err -> pure (Left err)
         Right new_ctx -> put new_ctx >> pure (Right ())
 
--- insert_local :: BindMayExist -> Bound -> State LocalScope (Either TypeError ())
 insert_local :: BindMayExist -> Bound -> Checker ()
 insert_local modifiable bound = do
     ctx <- get
