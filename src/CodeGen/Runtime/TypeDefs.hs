37fa418515b121f30b21a85ef265f99ae6a3c276
dan soucy <dev@danso.ca>
2021-05-12 03:19:36 -0400

    generate none object

--- src/CodeGen/Runtime/TypeDefs.hs
+++ src/CodeGen/Runtime/TypeDefs.hs
@@ -3,10 +3,20 @@ module CodeGen.Runtime.TypeDefs (typedefs) where
 import Data.List (intercalate)
 
 typedefs :: String
-typedefs = intercalate "\n" [souc_type_tag, souc_val, souc_obj]
+typedefs = intercalate "\n" [
+    souc_type_tag,
+    souc_val,
+    souc_obj,
+    souc_maybe,
+    souc_none
+    ]
 
 souc_type_tag = "enum _souc_type_tag { _souc_int , _souc_char , _souc_str };"
 
 souc_val = "union _souc_val { int _souc_int ; char _souc_char ; char * _souc_str; };"
 
 souc_obj = "struct _souc_obj { enum _souc_type_tag type ; union _souc_val val; };"
+
+souc_maybe = "struct _souc_maybe { int has_val; struct _souc_obj val; };"
+
+souc_none = "struct _souc_maybe _souc_none = { 0 };"
