0372ccaf8ebd04a760ec99aed085380694a7324b
dan soucy <dev@danso.ca>
2021-05-18 00:46:43 -0400

    leave a note: try to do this without allocating

--- src/CodeGen/Runtime/FuncDefs.hs
+++ src/CodeGen/Runtime/FuncDefs.hs
@@ -9,5 +9,6 @@ funcdefs = intercalate "\n" [
     ]
 
 tuple = "union _souc_obj _souc_tuple(union _souc_obj x, union _souc_obj y) { struct _souc_pair * p = calloc(sizeof(*p), 1); p->first = x; p->second = y; return (union _souc_obj) { ._souc_pair = p }; }"
+-- FIXME can this be done without calloc?
 
 increment = " int _souc_increment(int n) { return n+1;} "
