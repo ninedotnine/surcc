3e39db12a5c3161b544dae349ff2a9f7f6fb060d
dan soucy <dev@danso.ca>
2021-05-21 01:12:09 -0400

    refactor generate_expr

--- src/CodeGen/ExprGen.hs
+++ src/CodeGen/ExprGen.hs
@@ -20,24 +20,23 @@ generate_expr (Leaf e) = generate_term e
 generate_expr (Signed e _) = generate_expr e
 generate_expr (Twig op e) = generate_prefix_expr op ++ generate_expr e
 generate_expr (Branch op x y) = case op of
-    Plus              ->  "_souc_sum(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    Minus             ->  "_souc_difference(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    Splat             ->  "_souc_product(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    Divide            ->  "_souc_quotient(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
+    Plus              ->  gen_call "_souc_sum(" x y
+    Minus             ->  gen_call "_souc_difference(" x y
+    Splat             ->  gen_call "_souc_product(" x y
+    Divide            ->  gen_call "_souc_quotient(" x y
     FloorDiv          ->  undefined
-    Modulo            ->  "_souc_remainder(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
+    Modulo            ->  gen_call "_souc_remainder(" x y
     Hihat             ->  undefined -- FIXME C doesn't have ^
-    Equals            ->  "_souc_is_equal(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    NotEquals         ->  "_souc_is_unequal(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
+    Equals            ->  gen_call "_souc_is_equal(" x y
+    NotEquals         ->  gen_call "_souc_is_unequal(" x y
     RegexMatch        ->  undefined
-    GreaterThan       ->  "_souc_is_greater(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    LesserThan        ->  "_souc_is_lesser(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    And               ->  "_souc_conjunction(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
-    Or                ->  "_souc_disjunction(" ++ generate_expr x ++ "," ++ generate_expr y ++ ")"
+    GreaterThan       ->  gen_call "_souc_is_greater(" x y
+    LesserThan        ->  gen_call "_souc_is_lesser(" x y
+    And               ->  gen_call "_souc_conjunction(" x y
+    Or                ->  gen_call "_souc_disjunction(" x y
     Xor               ->  undefined
     In                ->  undefined
-    Tuple             ->  "_souc_tuple(" ++ generate_expr x ++ "," ++
-                            generate_expr y ++ ")"
+    Tuple             ->  gen_call "_souc_tuple(" x y
     Iff               ->  undefined
     FromMaybe         ->  undefined
     Prepend           ->  undefined
@@ -56,6 +55,8 @@ generate_expr (Branch op x y) = case op of
     BindRight         ->  undefined
     BindLeft          ->  undefined
 
+gen_call :: String -> ASTree -> ASTree -> String
+gen_call s x y = s ++ generate_expr x <> "," <> generate_expr y ++ ")"
 
 generate_term :: Term -> String
 generate_term (LitInt l) = "(union _souc_obj) { ._souc_int = " ++ show l ++ "}"
