d328ec034f4cf4e4b13b2b3699b81efb7f063666
dan soucy <dev@danso.ca>
2021-06-04 02:52:24 -0400

    further parsing dedups

--- src/Parser/TabChecker.hs
+++ src/Parser/TabChecker.hs
@@ -1,8 +1,9 @@
 module Parser.TabChecker (check_tabs) where
 
-import Text.Parsec hiding (char, space, spaces, string)
-import qualified Text.Parsec as Parsec
+import Text.Parsec hiding (space, spaces, string, newline)
 import Text.Parsec.Error
+
+import Common.Parsing
 
 type TabCheckingParser a = Parsec String () a
 
@@ -26,11 +27,5 @@ blank_line = try (many space_or_tab *> newline) *> pure ()
 comment_line :: TabCheckingParser ()
 comment_line = (skipMany space_or_tab *> comment_leader) *> manyTill anyChar newline *> pure ()
 
-space_or_tab :: TabCheckingParser ()
-space_or_tab = (char ' ' <|> char '\t')
-
 comment_leader :: TabCheckingParser ()
-comment_leader = char ';'
-
-char :: Char -> TabCheckingParser ()
-char c = Parsec.char c *> pure () <?> ""
+comment_leader = char ';' *> pure ()
