d555c57ef5b3f56e6f6ef532c462cf17781f4fbc
dan soucy <dev@danso.ca>
2023-01-09 19:45:17 -0500

    lay groundwork for cleanups
    when a value is created, it might require a cleanup
    e. g. if it lives on the heap

--- src/SurCC/Builtins.hs
+++ src/SurCC/Builtins.hs
@@ -14,7 +14,15 @@ import TextShow (TextShow(..))
 import SurCC.Common
 
 
-type Mapping a = Map.HashMap a (Text, SoucType)
+type Mapping a = Map.HashMap a Builtin
+
+data Builtin = Builtin {
+    body :: Text,
+    souc_type :: SoucType,
+    cleanup :: Maybe Cleanup
+}
+
+newtype Cleanup = Cleanup Text
 
 newtype BuiltinsCtx = Builtins [Bound]
 
@@ -27,52 +35,86 @@ typeof_builtin = \case
     Right c -> typeof_builtin_data c
 
 gen_builtin_identifier :: Identifier -> Maybe Text
-gen_builtin_identifier i = fst <$> Map.lookup i builtins where
+gen_builtin_identifier i = body <$> Map.lookup i builtins where
+    builtins :: Mapping Identifier
     builtins = builtin_subroutines <>  builtin_functions <> builtin_constants
 
 typeof_builtin_identifier :: Identifier -> Maybe SoucType
-typeof_builtin_identifier i = snd <$> Map.lookup i builtins where
+typeof_builtin_identifier i = souc_type <$> Map.lookup i builtins where
+    builtins :: Mapping Identifier
     builtins = builtin_subroutines <> builtin_functions <> builtin_constants
 
 
 gen_builtin_data :: Constant -> Maybe Text
-gen_builtin_data c = fst <$> Map.lookup c builtin_data
+gen_builtin_data c = body <$> Map.lookup c builtin_data
 
 typeof_builtin_data :: Constant -> Maybe SoucType
-typeof_builtin_data c = snd <$> Map.lookup c builtin_data
+typeof_builtin_data c = souc_type <$> Map.lookup c builtin_data
 
 builtin_subroutines :: Mapping Identifier
 builtin_subroutines = Map.fromList [
-    ("puts", ("_souc_puts", (SoucRoutn SoucString)))
-    ,
-    ("write", ("_souc_write", SoucRoutn (SoucPair (SoucType "OutputStream" (SoucKind 0)) SoucString)))
-    ,
-    ("abort", ("abort", SoucIO))
-    ]
+    ("puts", Builtin "_souc_puts"
+        (SoucRoutn SoucString)
+        Nothing
+    ),
+    ("write", Builtin "_souc_write"
+        (SoucRoutn
+            (SoucPair (SoucType "OutputStream" (SoucKind 0)) SoucString))
+        Nothing
+    ),
+    ("abort", Builtin "abort"
+        SoucIO
+        Nothing
+    )]
 
 builtin_functions :: Mapping Identifier
 builtin_functions = Map.fromList [
-    ("increment", ("_souc_increment", SoucFn SoucInteger SoucInteger))
-    ,
-    ("str", ("_souc_int_str", SoucFn SoucInteger SoucString))
-    ]
+    ("increment", Builtin "_souc_increment"
+        (SoucFn SoucInteger SoucInteger)
+        Nothing
+    ),
+    ("str", Builtin "_souc_int_str"
+        (SoucFn SoucInteger SoucString)
+        Nothing
+    )]
 
 builtin_constants :: Mapping Identifier
 builtin_constants = Map.fromList [
-    ("pi", ("(union _souc_obj) { ._souc_int = 3 }", SoucInteger)) -- biblical value
-    ,
-    ("i42", ("_souc_42", SoucInteger))
-    ,
-    ("ok43", ("_souc_42", SoucInteger))
-    ]
+    ("pi", Builtin
+        "(union _souc_obj) { ._souc_int = 3 }" -- biblical value
+        SoucInteger
+        Nothing
+    ),
+    ("i42", Builtin
+        "_souc_42"
+        SoucInteger
+        Nothing
+    ),
+    ("ok43", Builtin
+        "_souc_42"
+        SoucInteger
+        Nothing
+    )]
 
 builtin_data :: Mapping Constant
 builtin_data = Map.fromList [
-    ("True", ("(union _souc_obj) { ._souc_bool = true }", SoucType "Bool" (SoucKind 0)))
-    ,
-    ("False", ("(union _souc_obj) { ._souc_bool = false }", SoucType "Bool" (SoucKind 0)))
-    ,
-    ("None", ("_souc_none", SoucMaybe SoucInteger))
-    ,
-    ("OK", ("_souc_ok", SoucFn SoucInteger (SoucMaybe SoucInteger)))
-    ]
+    ("True", Builtin
+        "(union _souc_obj) { ._souc_bool = true }"
+        (SoucType "Bool" (SoucKind 0))
+        Nothing
+    ),
+    ("False", Builtin
+        "(union _souc_obj) { ._souc_bool = false }"
+        (SoucType "Bool" (SoucKind 0))
+        Nothing
+    ),
+    ("None", Builtin
+        "_souc_none"
+        (SoucMaybe SoucInteger)
+        Nothing
+    ),
+    ("OK", Builtin
+        "_souc_ok"
+        (SoucFn SoucInteger (SoucMaybe SoucInteger))
+        Nothing
+    )]
