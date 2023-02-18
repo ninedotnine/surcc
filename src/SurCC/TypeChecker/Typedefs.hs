c02b54cb8b301152d50f2a353acc900883d736ca
danso <dev@danso.ca>
2023-02-17 20:37:05 -0500

    simplify build_typedef

--- src/SurCC/TypeChecker/Typedefs.hs
+++ src/SurCC/TypeChecker/Typedefs.hs
@@ -31,39 +31,33 @@ build_typedefs' :: (TypeSet, Mapping) -> [Bound] -> [TypeDef]
                    -> Either TypeError (GlobalScope,[Bound])
 build_typedefs' (types, consts) bounds = \case
     (d:defs) -> do
-        (t, refut, terms) <- build_typedef d
+        let (t, refut, terms) = build_typedef d
         updated_types <- insert_type types t refut
-        updated_consts <- insert_consts consts terms t
-        let abounds = (terms <&> (\term -> Bound term t)) <> bounds
+        updated_consts <- insert_consts consts terms
+--         let abounds = (terms <&> (\term -> Bound term t)) <> bounds
+        let abounds = terms <> bounds
         build_typedefs' (updated_types, updated_consts) abounds defs
     [] -> pure $ (GlobalScope types consts, bounds)
 
 
--- FIXME can this function fail? does it need Either TypeError?
-build_typedef :: TypeDef
-                 -> Either TypeError (SoucType, Refutable, [Identifier])
-build_typedef = pure <$> \case
+build_typedef :: TypeDef -> (SoucType, Refutable, [Bound])
+build_typedef = \case
     EmptyType t -> (t, Refutable True, [])
-    UnitType t term -> (t, Refutable False, [term])
+    UnitType t term -> (t, Refutable False, [Bound term t])
     SynonymType t0 t1 -> undefined t0 t1 -- FIXME
-    WrapperType t0 constructor t1 ->
-        (undefined t0 t1, Refutable False, [constructor]) -- FIXME
+    WrapperType t0 constructor t1 -> -- FIXME
+        (undefined t0 t1, Refutable False, [Bound constructor (SoucFn t1 t0)])
     EnumType t constructors ->
-        -- FIXME fail if two constructors are equal
-        (t, Refutable True, constructors) -- FIXME
-    StructType t fns ->
-        -- FIXME we're gonna need the bounds i think
-        (t, Refutable False, fns <&> get_id)
-        where get_id (Bound i _) = i
+        (t, Refutable True, (constructors <&> (\con -> Bound con t)))
+    StructType t fns -> (t, Refutable False, fns)
     GADType t -> undefined t -- FIXME
 
 
-insert_consts :: Mapping -> [Identifier] -> SoucType
-                 -> Either TypeError Mapping
-insert_consts m i t = case i of
-    (con:cons) -> do
-        new_map <- insert_const m con t
-        insert_consts new_map cons t
+insert_consts :: Mapping -> [Bound] -> Either TypeError Mapping
+insert_consts m bounds = case bounds of
+    ((Bound i t):cons) -> do
+        new_map <- insert_const m i t
+        insert_consts new_map cons
     [] -> pure m
 
 
